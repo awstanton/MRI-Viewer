9e407783f58076ec344b6e8889d94daa4f9dd2c4
Andrew <dstanto2@mailfence.com>
2022-12-24 16:19:47 -0700

    Add open dialog

--- MRIViewerFrame.cpp
+++ MRIViewerFrame.cpp
@@ -1,29 +1,39 @@
 #include "MRIViewerFrame.h"
 
+void MRIViewerFrame::openMenu(wxCommandEvent& WXUNUSED(event)) {
+    wxFileDialog* OpenDialog = new wxFileDialog(
+		this, _("Choose a file to open"), wxEmptyString, wxEmptyString,
+		wxT("Text files (*.txt)|*.txt|C++ Source Files (*.cpp, *.cxx)|*.cpp;*.cxx| C Source files (*.c)|*.c|C header files (*.h)|*.h"), wxFD_OPEN, wxDefaultPosition);
+    if (OpenDialog->ShowModal() == wxID_OK) // if the user click "Open" instead of "Cancel"
+	{
+		CurrentDocPath = OpenDialog->GetPath();
+		// Sets our current document to the file the user selected
+		MainEditBox->LoadFile(CurrentDocPath); //Opens that file
+		SetTitle(wxString("Edit - ") << OpenDialog->GetFilename()); // Set the Title to reflect the file open
+	}
+
+	// Clean up after ourselves
+	OpenDialog->Destroy();
+}
+
 
 MRIViewerFrame::MRIViewerFrame() : wxFrame(NULL, wxID_ANY, "Hello World")
 {
     wxMenu *fileMenu = new wxMenu;
-    wxMenu *helpMenu = new wxMenu;
     wxMenu *mainMenu = new wxMenu;
     wxMenuBar *mainMenuBar = new wxMenuBar;
 
-    fileMenu->Append(ID_Hello, "&Hello...\tCtrl-H", "Help string", wxITEM_NORMAL);
-    fileMenu->AppendSeparator();
+    fileMenu->Append(wxID_ABOUT);
     fileMenu->Append(wxID_EXIT);
 
-    helpMenu->Append(wxID_ABOUT);
+    mainMenu->Append(wxID_OPEN, wxT("&Open File"));
+    mainMenu->Bind(wxEVT_MENU, &MRIViewerFrame::openMenu, this, wxID_OPEN);
 
-    mainMenuBar->Append(fileMenu, "File");
-    mainMenuBar->Append(helpMenu, "Help");
 
-    SetMenuBar(mainMenuBar);
-
-    mainMenu->Append(1, wxT("Open File"));
+    mainMenuBar->Append(fileMenu, wxT("&File"));
     mainMenuBar->Append(mainMenu, wxT("Files"));
 
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
+    SetMenuBar(mainMenuBar);
 
     Bind(wxEVT_MENU, &MRIViewerFrame::OnHello, this, ID_Hello);
     Bind(wxEVT_MENU, &MRIViewerFrame::OnAbout, this, wxID_ABOUT);
--- MRIViewerFrame.h
+++ MRIViewerFrame.h
@@ -1,17 +1,21 @@
 #include <wx/wxprec.h>
+#include <wx/filedlg.h>
 
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
 
-enum { ID_Hello = 1 };
+enum { ID_Hello = 1, MENU_Open };
 
 class MRIViewerFrame : public wxFrame
 {
 public:
     MRIViewerFrame();
+    wxString CurrentDocPath;
+    wxTextCtrl *MainEditBox;
 
 private:
+    void openMenu(wxCommandEvent& event);
     void OnHello(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
